a64acc21cc46640d758b58a2f0f53b0b2b96172b
ewi josepha <ewijosepha@gmail.com>
2023-10-13 14:02:04 +0200

    ui upload

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,52 +1,76 @@
-let secondfield = document.getElementById('input-field')
-let firstfield = document.getElementById('field-input')
-let selectedMeasure = document.getElementById('first-measure')
-let selectedMeasuretwo = document.getElementById('second-measure')
+let secondfield = document.getElementById("input-field");
+let firstfield = document.getElementById("field-input");
+let selectedMeasure = document.getElementById("first-measure");
+// let selectedMeasuretwo = document.getElementById("second-measure")
 
 
-firstfield.addEventListener('change', (e)=>{
-  const fieldValue = e.target.value
+const measurements = [
+
+  {
+    name: "meters",
+    value: 45
+  },
+  {
+    name: "Ferenheit",
+    value: 5
+  
+  },
+  {
+    name: "celsuis",
+    value: 4
+  }
+]
+
+const option = document.createElement('option')
+  selectedMeasure.append(option)
+
+  selectedMeasure.innerHTML = measurements.map(
+    (item) =>
+      `<option value=${item.name}>${item.name}</option>`
+  )
+
+
+firstfield.addEventListener("change", (e) => {
+  const fieldValue = e.target.value;
   console.log(fieldValue);
 
-function vals () {
-  let measureselected = selectedMeasure.value
-  let selectedval = measureselected.length
-  console.log(selectedval);
-}
+  let measureselected;
+  selectedMeasure.addEventListener("click", () => {
+    measureselected = selectedMeasure.value
+    console.log(measureselected);
+    choosenMeasurement = measureselected.split()
+ 
+    // let selectedval = measureselected.length;
+    console.log(measureselected);
+    
+  });
 
-selectedMeasure.addEventListener('change', vals())
+  // convertion from celsuis to farenheit
+
+  function celsuisToFahrenheit() {
+    // getting the unit measure selected fron the dropdown
 
 
+    // convertion
 
-function celsuisToFahrenheit(nam){
-  // let temp= 30
-  // console.log(temp*1.8+32+"°F" )
-  let far = fieldValue * 1.8 + 32 + "°f"
-
-  if(nam=== Ferenheit){
+    let far = fieldValue * 1.8 + 32 + "°f";
     console.log(far);
-
-     return far
+    return far;
   }
+  celsuisToFahrenheit();
 
-}
-  celsuisToFahrenheit('measureselected')
 
-  function farhenheitCelsuis(){
-   
-    let celsuis=(fieldValue-32)*5/9+"°c"
+  // convertion from farenheit to celsuis
+
+  function farhenheitCelsuis() {
+    let celsuis = ((fieldValue - 32) * 5) / 9 + "°c";
     console.log(celsuis);
-    return celsuis
+    return celsuis;
   }
-  farhenheitCelsuis()
-})
+  farhenheitCelsuis();
+});
 
-
-
-
-secondfield.addEventListener('change', (e)=>{
-const secondfieldfieldValue = e.target.value
-console.log(secondfieldfieldValue);
-
-})
-
+// secondfield.addEventListener("change", (e) => {
+//   const secondfieldfieldValue = e.target.value;
+//   console.log(secondfieldfieldValue);
+// });
